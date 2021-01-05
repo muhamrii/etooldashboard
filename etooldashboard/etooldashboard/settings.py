07ebf05ded3582cadd3e68f211eeb62e611234bc
muhamrii <muhamri1998@gmail.com>
2021-01-05 12:50:39 +0700

    add homev6

--- etooldashboard/etooldashboard/settings.py
+++ etooldashboard/etooldashboard/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'etooldashboard.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(PROJECT_PATH, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
