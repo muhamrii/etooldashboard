4eebe7c7ea4fa9186bb2a6e370ab1d71819ec1a3
passerver <etool.proactivesupport@ericsson.com>
2021-01-05 12:12:11 +0700

    add home

--- .env/bin/django-admin.py
+++ .env/bin/django-admin.py
@@ -1,4 +1,4 @@
-#!/home/etool/eanasep/muhamrii/dashboard/etooldashboard/.env/bin/python
+#!/home/etool/eanasep/muhamrii/dashboard/etooldashboard/.env/bin/python3.6
 # When the django-admin.py deprecation ends, remove this script.
 import warnings
 
