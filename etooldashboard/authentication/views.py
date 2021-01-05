e33df0a34723f98ab2684d3268618cb0c791bef2
muhamrii <muhamri1998@gmail.com>
2021-01-05 14:53:54 +0700

    perloginan

--- etooldashboard/authentication/views.py
+++ etooldashboard/authentication/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
-# Create your views here.
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.models import User
