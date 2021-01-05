f7d69cb17138c4f3f3388780c18542a20da8f23e
muhamrii <muhamri1998@gmail.com>
2021-01-05 12:45:56 +0700

    add homev5

--- etooldashboard/etooldashboard/settings.py
+++ etooldashboard/etooldashboard/settings.py
@@ -116,6 +116,10 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/3.1/howto/static-files/
-
+# https://docs.djangoproject.com/en/2.2/howto/static-files/
+STATICFILES_LOCATION = 'static'
 STATIC_URL = '/static/'
+STATIC_ROOT = 'static'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'etooldashboard/static')
+]
